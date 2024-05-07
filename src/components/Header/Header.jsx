1b93d423c8b9f244207d283653fc9bf6041f79c9
denysvysotskyi <denyvysotskyi@gmail.com>
2024-05-08 01:09:13 +0300

    Modificated Main and Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import './Header.css'
+import logo from '../../images/logo.svg'
+
+const time = new Date().toLocaleTimeString()
 
 const Header = () => (
   <header>
+    <img src={ logo } alt="logo" />
+    <span> Тут буде час: { time } </span>
   </header>
 )
 
