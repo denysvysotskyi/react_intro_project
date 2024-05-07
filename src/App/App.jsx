1bcbb8d6e5c310d252f9145f0daecd93004613d5
denysvysotskyi <denyvysotskyi@gmail.com>
2024-05-07 21:22:02 +0300

    Add header

---
+++ src/App/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from '../Header/Header'
+
+const App = () => (
+    <div>
+      <Header />
+
+
+      <main>
+        <h1> Hello React!! </h1>
+      </main>
+    </div>
+)
+
+export default App
