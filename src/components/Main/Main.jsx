16abae52ee3d03d0af72b7d0893e76733fffaec5
denysvysotskyi <denyvysotskyi@gmail.com>
2024-05-09 22:21:20 +0300

    Add useState for buttons

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,24 +1,37 @@
 import './Main.css'
 import List from '../List/List'
 import { data } from '../Data/data'
+import Button from '../Button/Button'
+import { useState } from 'react'
 
-const Main = () => (
-  <main>
-    <section>
-      <h3> Наші переваги </h3>
+  const Main = () => {
+    const [ content, setContent ] = useState('Click on your button')
+    const handleClick = type => setContent(type)
 
-      <ul>
-        <List title = { data[0].title } description = { data[0].description }/>
-        <List { ...data[1] } />
-        <List { ...data[2] } />
-        <List { ...data[3] } />
-      </ul>
-    </section>
-    <section>
-      <h3> Чим ми відрізняємось від інших </h3>
+    return (
+      <main>
+        <section>
+          <h3> Наші переваги </h3>
 
-    </section>
-  </main>
-)
+          <ul>
+            <List title = { data[0].title } description = { data[0].description }/>
+            <List { ...data[1] } />
+            <List { ...data[2] } />
+            <List { ...data[3] } />
+          </ul>
+        </section>
+        <section>
+          <h3> Чим ми відрізняємось від інших </h3>
+
+          <Button onClick={ () => handleClick('Clicked button 1') }> Click 1</Button>
+          <Button onClick={ () => handleClick('Clicked button 2') }> Click 2</Button>
+          <Button onClick={ () => handleClick('Clicked button 3') }> Click 3</Button>
+
+          <p> { content } </p>
+
+        </section>
+      </main>
+    )
+}
 
 export default Main
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import './main.css'
+import './style.css'
 import App from './components/App/App'
 
 const root = document.getElementById('root')
